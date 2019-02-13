124630c291489608c0fb7e7c062f5ea1747bb73b
m053m716 <max.murphy11@gmail.com>
2019-02-13 12:18:52 -0600

    Add initial code to do simple median correction. -MM

---
+++ +mbf/calibrate.m
@@ -0,0 +1,136 @@
+function fig = calibrate(image_folder)
+%% CALIBRATE   Do calibration (correction) on image stack
+%
+%   mbf.calibrate;
+%   fig = mbf.calibrate(image_folder);
+%
+%  --------
+%   INPUTS
+%  --------
+%  image_folder   :     (Optional) Full path of image folder with image
+%                          stack to do calibration. (e.g.
+%                          'C:/Pictures/Data')
+%
+%                             -> Can be given as a cell array to do
+%                                   multiple at once.
+%
+%  --------
+%   OUTPUT
+%  --------
+%  fig            :     Handle to UI for figure to preview how correction
+%                          looks.
+%
+%  Creates a new folder in the parent directory of image_folder with the
+%  same name as image_folder with '_out' appended. Image stack in that
+%  folder will have the correction applied.
+%
+% By: Max Murphy  v1.0  2019-02-13  Original version (R2017a)
+
+%% PARSE INPUT
+if nargin < 1
+   image_folder = uigetdir('C:\','Select image stack folder');
+   if image_folder == 0
+      error('No folder selected. Calibration canceled.');
+   end
+end
+
+if iscell(image_folder)
+   fig = cell(size(image_folder));
+   for ii = 1:numel(image_folder)
+      fig{ii} = calibrate(image_folder{ii});
+   end
+   return;
+end
+
+%% GET DIMENSIONS
+F = dir(fullfile(image_folder,'*'));
+F = F(~[F.isdir]); % Remove any folder elements only want files
+
+if isempty(F)
+   error('No images located in that folder (%s). Is it correct?',...
+      image_folder);
+end
+
+A = imread(fullfile(image_folder,F(1).name));
+[h,w] = size(A);
+
+%% IF CORRECTION EXISTS, LOAD IT, OTHERWISE PROMPT FOR CORRECTION
+p = mfilename('fullpath');
+[funcDir,~,~] = fileparts(p);
+
+calFile = fullfile(funcDir,sprintf('cal-%05gx%05g.mat',h,w));
+if exist(calFile,'file')==0
+   if ~getCorrection(calFile)
+      error('Unable to obtain correction file. Calibration canceled.');
+   end
+end
+cal = load(calFile,'img');
+
+%% APPLY THE CORRECTION AND WRITE TO OUTPUT FOLDER
+out_folder = fullfile([image_folder '_out']);
+if exist(out_folder,'dir')==0
+   mkdir(out_folder);
+   
+else  % If it already exists, delete and make new (re-apply calibration)
+   delete(out_folder);
+   mkdir(out_folder);
+end
+
+fprintf(1,'Performing correction...%03g%%\n',0);
+pct = 0;
+N = numel(F);
+for ii = 1:N
+   A = imread(fullfile(image_folder,F(ii).name));
+   B = A - cal.img; % Just subtract correction
+   imwrite(B,fullfile(out_folder,F(ii).name));
+   
+   % Only overwrite when percentage increases:
+   frac_done = floor(ii/N * 100); 
+   if frac_done > pct
+      pct = frac_done;
+      fprintf(1,'\b\b\b\b\b%03g%%\n',pct);
+   end
+end
+
+%% SHOW CORRECTION FIGURE
+fig = figure('Name',sprintf('%s: Calibration Preview',image_folder),...
+   'Units','Normalized',...
+   'Color','w',...
+   'Position',[0.1 0.1 0.8 0.8]);
+
+axA = axes(fig,'Units','Normalized',...
+   'Position',[0.05 0.05 0.425 0.9],...
+   'Color','w',...
+   'XColor','w',...
+   'XLimMode','manual',...
+   'XLim',[0 1],...
+   'YColor','w',...
+   'YLimMode','manual',...
+   'YLim',[0 1],...
+   'YDir','reverse',...
+   'FontName','Arial',...
+   'XTick',[],...
+   'YTick',[],...
+   'NextPlot','add');
+
+axB = axes(fig,'Units','Normalized',...
+   'Position',[0.525 0.05 0.425 0.9],...
+   'Color','w',...
+   'XColor','w',...
+   'XLimMode','manual',...
+   'XLim',[0 1],...
+   'YColor','w',...
+   'YLimMode','manual',...
+   'YLim',[0 1],...
+   'YDir','reverse',...
+   'FontName','Arial',...
+   'XTick',[],...
+   'YTick',[],...
+   'NextPlot','add');
+
+imagesc(axA,[0 1],[0 1],A);
+imagesc(axB,[0 1],[0 1],B);
+
+
+end
+
---
+++ +mbf/getCorrection.m
@@ -0,0 +1,100 @@
+function flag = getCorrection(calFile)
+%% GETCORRECTION  Get correction offset to apply for calibration of MBF
+%
+%  flag = mbf.getCorrection;
+%  flag = mbf.getCorrection(calFile);
+%
+%  --------
+%   INPUTS
+%  --------
+%   calFile    :     (optional) Calibration file name. If specified, then
+%                       the first image in image stack selected for
+%                       obtaining the correction MUST be the same dimension
+%                       as parsed from the calibration filename. Format is
+%                       'cal-hhhhhxwwwww.mat' where h is height character,
+%                       w is width character
+%
+%  --------
+%   OUTPUT
+%  --------
+%    flag      :     Flag indicating successful completion.
+%
+% By: Max Murphy  v1.0  2019-02-13  Original version (R2017a)
+
+%% INITIALIZE VARIABLES
+flag = false;
+h = nan;
+w = nan;
+
+%% PARSE INPUT
+if nargin == 1
+   [~,calFile,~] = fileparts(calFile);
+   idx = regexp(calFile,'\d\d\d\d\d');
+   h = str2double(calFile(idx(1):(idx(1)+4)));
+   w = str2double(calFile(idx(2):(idx(2)+4)));
+else
+   calFile = 'cal-%05gx%05g.mat';
+end
+
+%% GET CORRECTION IMAGE STACK
+correction_folder = uigetdir('C:\',...
+   'Select folder with CORRECTION image stack');
+if correction_folder == 0
+   warning('No CORRECTION folder selected.');
+   return;
+end
+
+%% CHECK THAT IT IS A VALID FOLDER
+F = dir(fullfile(correction_folder,'*'));
+F = F(~[F.isdir]); % Remove sub-directories
+% Get size of first image in that image stack
+a = imread(fullfile(F(1).folder,F(1).name));
+[hh,ww,~] = size(a);
+if ~isnan(h) && ~isnan(w) % Then check dimensions of first image
+   returnFlag = false;
+   if ~(h == hh)
+      warning('Bad HEIGHT dimension (%g; should be %g).',hh,h);
+      returnFlag = true;
+   end
+   
+   if ~(w == ww)
+      warning('Bad WIDTH dimension (%g; should be %g).',ww,w);
+      returnFlag = true;
+   end
+   
+   if returnFlag
+      return;
+   end
+else
+   calFile = sprintf(calFile,hh,ww);
+end
+
+%% EXTRACT OFFSET IMAGE AND SAVE IT
+img = extractCorrectionImage(F,a); 
+p = mfilename('full');
+[funcDir,~,~] = fileparts(p);
+save(fullfile(funcDir,calFile),'img','-v7.3');
+flag = true;
+
+%% SUB-FUNCTIONS
+   function img = extractCorrectionImage(F,a)
+      n = numel(F);
+      
+      img = zeros(size(a,1),size(a,2),n);
+      
+      fprintf(1,'Computing correction offset...%03g%%\n',0);
+      pct = 0;      
+      for i = 1:n
+         img(:,:,i) = imread(fullfile(F(i).folder,F(i).name));
+      
+         % Only overwrite when percentage increases:
+         frac_done = floor(i/n * 100); 
+         if frac_done > pct
+            pct = frac_done;
+            fprintf(1,'\b\b\b\b\b%03g%%\n',pct);
+         end
+      end
+      img = median(img,3); % Take median across all images for each pixel
+   end
+
+end
